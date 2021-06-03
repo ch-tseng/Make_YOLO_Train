2dd6e194e2b4a4dab46f4328a56e3f6fb03c9a69
ch-tseng <ch.tseng@sunplusit.com>
2021-06-03 09:26:04 +0800

    update

--- 3_anchors.py
+++ 3_anchors.py
@@ -1,20 +1,35 @@
 import os, subprocess
 
-cfgFolder = "/WORK1/dataset/crowd_human_water/try2/cfg_train"
+cfgFolder = "/WORKING/modelSale/forklift/cfg_train"
 dark_home = "/home/chtseng/frameworks/darknet.v4"
 sizes = [ [9,416], [9,512], [9,608], [9,640], [20,1536], [6,320], [6,416] ]
 #sizes = [ [9,608] ]
 
 
 darknet_path = os.path.join(dark_home, 'darknet')
+cmd_file = os.path.join(cfgFolder, 'train_cmd.txt')
 
-for i, size in enumerate(sizes):
-    print("=============================================================================================================")
-    print("YOLO anchors Caculating for {}x{}, {} boxes...................................................".format(\
-        sizes[i][1], sizes[i][1], sizes[i][0]))
-    print("=============================================================================================================")
-    subprocess.run([darknet_path, 'detector', 'calc_anchors', os.path.join(cfgFolder, 'obj.data'), '-num_of_clusters', \
-        str(sizes[i][0]), '-width', str(sizes[i][1]), '-height', str(sizes[i][1]) ])
+with open(cmd_file, 'w') as file:
+    for i, size in enumerate(sizes):
+        pline = "============================================================================================================="
+        print(pline)
+        file.write(pline+'\n')
 
+        pline = "[{}x{}, {} boxes]".format(\
+            sizes[i][1], sizes[i][1], sizes[i][0])
 
+        print(pline)
+        file.write(pline+'\n')
+
+        nmap_out = subprocess.run([darknet_path, 'detector', 'calc_anchors', os.path.join(cfgFolder, 'obj.data'), '-num_of_clusters', \
+            str(sizes[i][0]), '-width', str(sizes[i][1]), '-height', str(sizes[i][1])], universal_newlines=False, stdout=subprocess.PIPE)
+
+        nmap_lines = nmap_out.stdout.splitlines()
+        ans = ''
+        for line in nmap_lines:
+            if 'anchors =' in str(line):
+                ans = line.decode('ascii')
+                print(ans)
+
+        file.write(ans + '\n\n')
 
