8f75b55e3ded5506d681bdece92a1ed3ecebb503
ch-tseng <ch.tseng@sunplusit.com>
2021-08-10 14:33:16 +0800

    update

--- 4_make_model.py
+++ 4_make_model.py
@@ -1,8 +1,8 @@
 import os,sys
 
 classList = { "person_head":0, "person_vbox":1 }
-cfgFolder = "/WORKING/modelSale/crowd_human_sport/cfg_train"
 
+cfgFolder = "/WORKING/modelSale/crowd_human/cfg_train"
 '''
 classList = { "balaclava_ski_mask":0, "eyeglasses":1, "face_no_mask":2, "face_other_covering":3, "face_shield":4, \
               "face_with_mask":5, "face_with_mask_incorrect":6, "gas_mask":7, "goggles":8, "hair_net":9, "hat":10, \
@@ -10,23 +10,26 @@ classList = { "balaclava_ski_mask":0, "eyeglasses":1, "face_no_mask":2, "face_ot
               "scarf_bandana":17, "sunglasses":18, "turban":19 }
 '''
 dark_home = "/home/chtseng/frameworks/darknet"
+yolofastest_home = "/home/chtseng/frameworks/Yolo-Fastest"
 yolov5_home = "/home/chtseng/frameworks/yolov5"
 
 yolo_config = {
     'numBatch': 120,
     'numSubdivision': 40,
-    '416': "6,  7,   9, 23,  24, 10,  19, 53,  65, 22,  33,113, 142, 45,  68,196, 233,158",
-    '512': "7,  8,  12, 28,  30, 12,  23, 66,  80, 27,  41,140, 175, 56,  84,242, 287,195",
-    '608': "8, 10,  32, 13,  15, 36,  77, 27,  30, 91, 168, 50,  58,192, 299,111, 152,346",
-    '640': "9, 10,  15, 35,  38, 16,  29, 83, 101, 33,  51,175, 220, 70, 105,302, 359,245",
-    '1536': "16, 18,  21, 50,  51, 23,  39, 76,  99, 41,  34,150,  69,126, 191, 63,  60,274, 110,218, 244,127, 404, 85,  97,433, 212,364, 555,166, 153,626, 275,787, 799,304, 486,1029, 1104,683"
+    '416': "7,  5,   8, 20,  24,  9,  17, 48,  59, 19,  32,106, 125, 38,  73,194, 231,101",
+    '512': "9,  7,  28, 11,  12, 30,  62, 21,  25, 77, 129, 37,  56,174, 223, 76, 246,251",
+    '608': "10,  8,  33, 13,  14, 35,  75, 25,  30, 92, 154, 44,  67,207, 265, 90, 292,299",
+    '640': "12,  8,  12, 30,  38, 15,  25, 72,  92, 29,  49,162, 195, 59, 110,296, 357,158",
+    '768': "13, 10,  42, 16,  17, 45,  95, 32,  38,116, 195, 56,  84,262, 336,114, 369,378",
+    '960': "16, 13,  52, 20,  22, 55, 117, 39,  47,145, 242, 70, 105,327, 418,142, 460,471",
+    '1536': "19, 15,  47, 20,  21, 48,  68, 37, 125, 28,  37,102, 114, 58,  73,142, 228, 50,  58,260, 190, 97, 376, 84, 110,376, 304,184, 557,135, 186,597, 693,241, 364,825, 968,405, 1000,972"
 }
 
 yolotiny_config = {
     'numBatch': 32,
     'numSubdivision': 2,
-    '320': "6,  6,  10, 27,  28, 11,  24, 80,  86, 26, 102,141",
-    '416': "7,  8,  13, 35,  36, 14,  32,104, 112, 34, 132,183",
+    '320': "7,  5,  26,  9,   9, 26,  74, 22,  25, 82, 142, 89",
+    '416': "9,  7,  34, 12,  12, 34,  97, 29,  33,107, 185,115",
 }
 
 #---------------------------------------------------------------------
@@ -43,8 +46,8 @@ cfgs = {
     "yolov3-spp": ["cfg/yolov3-spp.cfg", "pretrained/yolov3-spp.weights", 608],
     "yolov4": ["cfg/yolov4.cfg", "pretrained/yolov4.conv.137", 608],
     "yolov4-tiny": ["cfg/yolov4-tiny.cfg", "pretrained/yolov4-tiny.conv.29", 416],
-    "yolo-fastest": ["cfg/yolo-fastest.cfg", "pretrained/yolo-fastest.weights", 320],
-    "yolo-fastest-xl": ["cfg/yolo-fastest-xl.cfg", "pretrained/yolo-fastest-xl.weights", 320],
+    "yolo-fastest": ["cfg/yolo-fastest-1.1.cfg", "pretrained/yolo-fastest.conv.109", 320],
+    "yolo-fastest-xl": ["cfg/yolo-fastest-1.1-xl.cfg", "pretrained/yolo-fastest-xl.conv.109", 320],
     "yolov4x-mish": ["cfg/yolov4x-mish.cfg", "pretrained/yolov4x-mish.conv.166", 640],
     "yolov4-csp": ["cfg/yolov4-csp.cfg", "pretrained/yolov4-csp.conv.142", 512],
     "yolov4-cspx-p7": ["cfg/cspx-p7-mish.cfg", "pretrained/cspx-p7-mish_hp.344.conv", 1536],
@@ -80,6 +83,7 @@ file.close
 tfile = open( os.path.join(cfgFolder, 'train_cmd.txt'), 'w')
 
 for cfg_name in cfgs:
+    print('test', cfg_name)
     with open(cfgs[cfg_name][0]) as file:
         file_content = file.read()
     file.close
@@ -131,8 +135,14 @@ for cfg_name in cfgs:
 
         cfg_file = cfg_name +'.cfg'
 
-        exec_cmd = "{}/darknet detector train \\\n    {} \\\n    {} \\\n    {} \\\n    -dont_show \\\n    -mjpeg_port 8090 \\\n    -clear \\\n    -gpus 0".format(\
-            dark_home, os.path.join(cfgFolder,'obj.data'), os.path.join(cfgFolder,cfg_name+'.cfg'), os.path.join(pwd,cfgs[cfg_name][1]))
+        if (cfg_name in ["yolo-fastest", "yolo-fastest-xl"]):
+            exec_cmd = "{}/darknet detector train \\\n    {} \\\n    {} \\\n    {} \\\n    -dont_show \\\n    -mjpeg_port 8090 \\\n    -clear \\\n    -gpus 0".format(\
+                yolofastest_home, os.path.join(cfgFolder,'obj.data'), os.path.join(cfgFolder,cfg_name+'.cfg'), os.path.join(pwd,cfgs[cfg_name][1]))
+
+        else:
+            exec_cmd = "{}/darknet detector train \\\n    {} \\\n    {} \\\n    {} \\\n    -dont_show \\\n    -mjpeg_port 8090 \\\n    -clear \\\n    -gpus 0".format(\
+                dark_home, os.path.join(cfgFolder,'obj.data'), os.path.join(cfgFolder,cfg_name+'.cfg'), os.path.join(pwd,cfgs[cfg_name][1]))
+
 
     file = open(os.path.join(cfgFolder, cfg_file), "w")
     file.write(file_updated)
